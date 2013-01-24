d01db348e42900b72ddd2d9e462cc673c403ecdb
SCaddell@my.wctc.edu <>
2013-01-23 20:44:52 -0600

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,15 @@
+package firstgit;
+
+/**
+ *
+ * @author Stuart - HP AMD 10
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
